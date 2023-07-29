bd51be9383b6b351b61d9861880dd477cd8f62ac
michelleraj <57809798+michelleraj@users.noreply.github.com>
2023-07-29 16:54:17 -0400

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+--A query is written and executed to create a Retirement Titles table for employees who are born between January 1, 1952 and December 31, 1955.
+
 SELECT DISTINCT ON e.emp_no,
 e.first_name,
 e.last_name, t.title, t.from_date, t.to_date
@@ -20,7 +22,7 @@ INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no)
 where dept_name='Sales' or  dept_name = 'Development';
 
-
+--Export the Unique Titles table as unique_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder
 SELECT DISTINCT on (e.emp_no)
  e.emp_no, e.first_name,
 e.last_name, ti.title, ti.from_date, ti.to_date
@@ -31,7 +33,7 @@ ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by e.emp_no asc, ti.to_date desc;
 
-
+--Write another query in the Employee_Database_challenge.sql file to retrieve the number of employees by their most recent job title who are about to retire.
 select count(title), title
 from unique_titles 
 group by title 
@@ -49,13 +51,14 @@ ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 order by e.emp_no asc, ti.to_date desc;
 
-
+--A query is written and executed to create a Retiring Titles table that contains the number of titles filled by employees who are retiring
 select count(title), title
 into retiring_titles
 from unique_titles 
 group by title 
 order by count desc
 
+--A query is written and executed to create a Mentorship Eligibility table for current employees who were born between January 1, 1965 and December 31, 1965. 
 SELECT DISTINCT on (e.emp_no)
 e.emp_no, e.first_name,
 e.last_name, e.birth_date, de.from_date, de.to_date,
